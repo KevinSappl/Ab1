3e2fb02ad464bef2622d16500a443fc71dc21ca6
k1sappl <sappl.kevke@gmx.at>
2023-05-08 18:41:05 +0200

    Added Heap implementation, not tested. Restructured.

---
+++ src/main/java/ab1/impl/Nachnamen/Ab1Impl.java
@@ -0,0 +1,34 @@
+package ab1.impl.Nachnamen;
+
+import ab1.Ab1;
+import ab1.Heap;
+import ab1.SortedList;
+import ab1.Sorter;
+
+public class Ab1Impl implements Ab1 {
+
+    @Override
+    public SortedList newSortedList() {
+        // Ihre Implementierung
+        return new SortedListImpl();
+    }
+
+    @Override
+    public Sorter newQuickSort() {
+        // Ihre Implementierung
+        return null;
+    }
+
+    @Override
+    public Heap newHeap(int capacity) {
+        // Ihre Implementierung
+        return new HeapImpl();
+    }
+
+    @Override
+    public Sorter newHeapSort(int capacity) {
+        // Ihre Implementierung
+        return null;
+    }
+
+}
---
+++ src/main/java/ab1/impl/Nachnamen/HeapImpl.java
@@ -0,0 +1,86 @@
+package ab1.impl.Nachnamen;
+
+import ab1.Heap;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+public class HeapImpl implements Heap {
+    private ArrayList<Integer> heap;
+    public HeapImpl() {
+        this.heap = new ArrayList<>();
+    }
+
+    @Override
+    public void clear() {
+        heap.clear();
+    }
+
+    @Override
+    public void add(int key) throws IndexOutOfBoundsException {
+        heap.add(key);
+        bubbleUp(heap.size()-1);
+    }
+
+    @Override
+    public int removeMax() throws NoSuchElementException {
+        if (heap.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        int max = heap.get(0);
+        int last = heap.remove(heap.size() - 1);
+        if (!heap.isEmpty()) {
+            heap.set(0, last);
+            bubbleDown(0);
+        }
+        return max;
+    }
+
+    @Override
+    public int max() throws NoSuchElementException {
+        if (heap.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return heap.get(0);
+    }
+
+    @Override
+    public int size() {
+        return heap.size();
+    }
+    //Helper Classes
+    private void bubbleDown(int index) {
+        int largestChildIndex = index;
+        while (true) {
+            int leftChildIndex = 2 * index + 1;
+            int rightChildIndex = 2 * index + 2;
+            if (leftChildIndex < heap.size() && heap.get(leftChildIndex) > heap.get(largestChildIndex)) {
+                largestChildIndex = leftChildIndex;
+            }
+            if (rightChildIndex < heap.size() && heap.get(rightChildIndex) > heap.get(largestChildIndex)) {
+                largestChildIndex = rightChildIndex;
+            }
+            if (largestChildIndex == index) {
+                break;
+            }
+            swap(index, largestChildIndex);
+            index = largestChildIndex;
+        }
+    }
+    private void bubbleUp(int index) {
+        while (index > 0) {
+            int parentIndex = (index - 1) / 2;
+            if (heap.get(index) <= heap.get(parentIndex)) {
+                break;
+            }
+            swap(index, parentIndex);
+            index = parentIndex;
+        }
+    }
+    private void swap(int i, int j) {
+        Integer temp = heap.get(i);
+        heap.set(i, heap.get(j));
+        heap.set(j, temp);
+    }
+    //
+}
